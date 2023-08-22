5e6f8757e12f791243e68141175dfe6d22a038be
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-22 17:00:29 +0530

    Try to add the swagger to the backend

--- index.js
+++ index.js
@@ -2,18 +2,43 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
+const swaggerjsdoc = require("swagger-jsdoc");
+const swaggerui = require("swagger-ui-express");
 
 const app = express();
 app.use(cors());
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info:{
+      title: "Interactive Book Reader",
+      version: "0.1.0",
+      description:
+        "This is the project that is being doing as Semester 5 Project.",
+      contact:{
+        name : "H.W.K.Aravinda",
+        email: "aravindahwk@gmail.com",
+      }
+    },
+    servers: [
+      {
+        url: "http://localhost:3001/",
+      },
+    ],
+  },
+  apis: ["./routes/book.js"],
+};
+
 app.use(morgan("dev"));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-app.use('/uploads',express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 
-const BookRoute=require('./routes/book')
-const AuthRoute=require('./routes/auth')
+const spacs = swaggerjsdoc(options);
+const BookRoute = require("./routes/book");
+const AuthRoute = require("./routes/auth");
 
 mongoose.connect(
   "mongodb+srv://Group3_SEP:TdxB2XR8PVZKJfvs@interactivebookreader.uscktdx.mongodb.net/Interactive_Book_Reader?retryWrites=true&w=majority",
@@ -33,12 +58,12 @@ db.once("open", () => {
   console.log("Database connection Established!");
 });
 
-
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is runing on port ${PORT}`);
 });
 
-app.use('/api/book',BookRoute)
-app.use('/api/publisher',AuthRoute)
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(spacs));
+app.use("/api/book", BookRoute);
+app.use("/api/publisher", AuthRoute);
