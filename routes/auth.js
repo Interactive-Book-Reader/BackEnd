dde276cb5a9b326a5c6cf320455b64dba4114ae8
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-29 22:19:40 +0530

    Add authenticate routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const AuthController = require("../controllers/AuthController");
+const authenticate = require("../middleware/authenticate");
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
-router.post("/update", AuthController.update);
+router.post("/update", authenticate,AuthController.update);
 router.post("/getPublisher", AuthController.getPublisher);
 router.post("/deletePublisher", AuthController.deletePublisher);
 
