d7f3fbb0b8dda98a635952c28ee98156b835d385
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-09-03 12:00:05 +0530

    Add clear email content

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -229,7 +229,51 @@ const sendOTPVerification = async({_id,email},res) => {
       from: process.env.AUTH_EMAIL,
       to: email,
       subject: 'OTP for Email Verification',
-      html: `<h1>OTP for Email Verification is <b>${otp}</b></h1>`
+      html: `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Email Verification</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  background-color: #f4f4f4;
+                  padding: 20px;
+              }
+              .container {
+                  max-width: 600px;
+                  margin: 0 auto;
+                  background-color: #ffffff;
+                  padding: 20px;
+                  border-radius: 5px;
+                  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                  color: #333;
+              }
+              p {
+                  font-size: 16px;
+                  line-height: 1.5;
+                  color: #666;
+              }
+              .otp {
+                  font-size: 24px;
+                  font-weight: bold;
+                  color: #007bff;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <h1>Email Verification</h1>
+              <p>Thank you for signing up. To complete your registration, please enter the OTP below:</p>
+              <p>Your OTP for Email Verification is: <span class="otp">${otp}</span></p>
+              <p>If you did not request this OTP, please ignore this email.</p>
+          </div>
+      </body>
+      </html>
+      `
     };
 
     const saltRounds = 10;
