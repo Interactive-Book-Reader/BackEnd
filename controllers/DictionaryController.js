be8d12ba6d13d291f3d68476b449156e3fbf585f
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-09-08 15:53:29 +0530

    Create dictionary controller

---
+++ controllers/DictionaryController.js
@@ -0,0 +1,101 @@
+const Dictinary = require("../models/Dictinary");
+
+const add = (req, res, next) => {
+  let dictinary = new Dictinary({
+    user_id: req.body.user_id,
+    word: req.body.word,
+    meaning: req.body.meaning,
+    example: req.body.example,
+  });
+  dictinary
+    .save()
+    .then(() => {
+      res.json({
+        message: "Word is added successfully.",
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: "An error is occured.",
+      });
+    });
+};
+
+const update = (req, res, next) => {
+    Dictinary.findOneAndUpdate({ _id: req.body._id }, {
+    $set: {
+      word: req.body.word,
+      meaning: req.body.meaning,
+      example: req.body.example,
+    },
+    })
+    .then(() => {
+        res.json({
+            message: "Word is updated successfully.",
+        });
+        }
+    )
+    .catch((error) => {
+        res.json({
+            message: "An error is occured.",
+        });
+        }
+    );
+};
+
+const remove = (req, res, next) => {
+    Dictinary.findByIdAndDelete({ _id: req.body._id })
+    .then(() => {
+        res.json({
+            message: "Word is deleted successfully.",
+        });
+        }
+    )
+    .catch((error) => {
+        res.json({
+            message: "An error is occured.",
+        });
+        }
+    );
+};
+
+const getAll = (req, res, next) => {
+    Dictinary.find({ user_id: req.body.user_id })
+    .then((data) => {
+        res.json({
+            data: data,
+        });
+        }
+    )
+    .catch((error) => {
+        res.json({
+            message: "An error is occured.",
+        });
+        }
+    );
+};
+
+const getOne = (req, res, next) => {
+    Dictinary.findById({ _id: req.body._id })
+    .then((data) => {
+        res.json({
+            data: data,
+        });
+        }
+    )
+    .catch((error) => {
+        res.json({
+            message: "An error is occured.",
+        });
+        }
+    );
+};
+
+module.exports = {
+    add,
+    update,
+    remove,
+    getAll,
+    getOne,
+};
+
