1eeaefd341248b0621dc6ec43ea346ff57305df1
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-09-12 09:25:09 +0530

    change the index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const BookRoute = require("./routes/book");
 const AuthRoute = require("./routes/auth");
 const UserRoute = require("./routes/user");
 const DicRoute = require("./routes/dictionary");
+const Read_BooksRoute = require("./routes/read_books");
 
 mongoose.connect(
   "mongodb+srv://Group3_SEP:TdxB2XR8PVZKJfvs@interactivebookreader.uscktdx.mongodb.net/Interactive_Book_Reader?retryWrites=true&w=majority",
@@ -71,3 +72,4 @@ app.use("/api/book", BookRoute);
 app.use("/api/publisher", AuthRoute);
 app.use("/api/user", UserRoute);
 app.use("/api/dictionary", DicRoute);
+app.use("/api/read_books", Read_BooksRoute);
